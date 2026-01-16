3bf9c83dfa0b69eca05aef669120ee4533acb00f
immelf2 <77293835+immelf2@users.noreply.github.com>
2026-01-16 01:32:46 -0500

    sync

--- types.ts
+++ types.ts
@@ -1,3 +1,4 @@
+//* API types
 export interface JobsItem {
   MatchedObjectDescriptor: {
     PositionTitle: string;
@@ -5,4 +6,21 @@ export interface JobsItem {
     PositionLocationDisplay: string;
     JobCategory?: { Name: string }[];
   };
+}
+
+export interface SearchResult {
+  SearchResultItems: JobsItem[];
+  SearchResultCount: number;
+}
+
+export interface JobSearchResult {
+  SearchResult: SearchResult;
+}
+
+//* internal types
+export interface Job {
+    title: string;
+    location: string;
+    department: string;
+    category: string;
 }
