836e34ec516aa7db1c9b48e2c32912d3fae750ce
immelf2 <77293835+immelf2@users.noreply.github.com>
2026-01-21 17:44:43 -0500

    fixed flag parsing bug

--- fetch.ts
+++ fetch.ts
@@ -4,6 +4,8 @@ import { type Flags, type Job, type JobsItem, type JobSearchResult, mapJobsItemT
 function parseFlags(args:string[]):Flags {
   const flags:Flags = {};
 
+  console.log(args);
+
   for (let i = 0; i < args.length; i++) {
     const arg = args[i];
 
@@ -16,23 +18,27 @@ function parseFlags(args:string[]):Flags {
       throw new Error(`missing value for ${key}`);
     }
 
-    if (key == 'k') flags.keyword = key;
-    if (key == 'keyword') flags.keyword = key;
-    if (key == 'l') flags.keyword = key;
-    if (key == 'location') flags.keyword = key;
+    if (key == 'r') flags.radius = value;
+    if (key == 'radius') flags.radius = value;
+    if (key == 'k') flags.keyword = value;
+    if (key == 'keyword') flags.keyword = value;
+    if (key == 'l') flags.location = value;
+    if (key == 'location') flags.location = value;
 
     i++;
   }
   return flags;
 }
 
-async function fetchJobs(keyword: string, location: string): Promise<Job[]> {
+async function fetchJobs(keyword: string, location: string, radius: string): Promise<Job[]> {
   const params = new URLSearchParams({
     Keyword: keyword,
     LocationName: location,
     ResultsPerPage: '50',
-    Radius: '100'
+    Radius: radius
   });
+
+  console.log(params.toString());
 
   const res: Response = await fetch(
     `https://data.usajobs.gov/api/Search?${params.toString()}`,
@@ -59,13 +65,16 @@ async function fetchJobs(keyword: string, location: string): Promise<Job[]> {
 async function main() : Promise<void> {
   const flags = parseFlags(process.argv.slice(2));
 
-  if (!flags.keyword || !flags.location) {
+  if (!flags.radius) flags.radius = "50";
+  if (!flags.keyword) flags.keyword = "Manager";
+  if (!flags.location) flags.location = "Charlotte, NC";
+  if (!flags.keyword || !flags.location || !flags.radius) {
     console.error(`Usage: node fetch.js --keyword <keyword> --location "<city, state>" or <ZIP>`);
     process.exit(1);
   }
 
   try {
-    const jobs = await fetchJobs(flags.keyword, flags.location);
+    const jobs = await fetchJobs(flags.keyword, flags.location, flags.radius);
     console.log(`found ${jobs.length} job(s)`);
     if (jobs.length > 0) {
       for (const job of jobs) {
